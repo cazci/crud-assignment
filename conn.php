17d391c8b4e94ced4840d8e5f45dcc74332b906d
Sashika Nawarathne <cazci269@gmail.com>
2019-03-18 11:46:15 +0530

    Db connection initialized

---
+++ conn.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbtuts";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
