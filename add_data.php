33ddb28c5fceb17f86816ac95b640013b7b6acc3
Sashika Nawarathne <cazci269@gmail.com>
2019-03-18 16:07:52 +0530

    Data 'create' function initialized

---
+++ add_data.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'conn.php';
+
+$name = $_POST['name'];
+$location = $_POST['location'];
+$contact = $_POST['contact'];
+$rating = $_POST['rating'];
+
+$sql = "INSERT INTO restaurants(name, location, contact, rating) VALUES ('$name', '$location', '$contact', '$rating')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
